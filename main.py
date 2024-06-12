dfd26a2d3bc1e8cd3e396694c9fff611070c6db2
Dory <pemegomachado@gmail.com>
2024-06-12 13:27:36 +0200

    Per-server transformations
    Now, whilst we can still use just one file per user, plus a global "transformed" file, you can have different transformations in different servers, at the same time!

--- main.py
+++ main.py
@@ -20,11 +20,8 @@ async def transform_function(ctx: discord.ApplicationContext,
     if len(into.strip()) <= 1:
         return await ctx.send("Please provide a name longer than 1 character!")
 
-    utils.write_tf(user, ctx.author.name, into, image_url)
-
-    with open("cache/transformed.txt", "r+") as f:
-        if user.name not in f.read():
-            f.write(f"{user.name}\n")
+    utils.write_tf(user, ctx.guild, ctx.author.name, into, image_url)
+    utils.write_transformed(user, ctx.guild)
 
 
 # Bot startup
@@ -44,24 +41,23 @@ async def on_message(message: discord.Message):
         return
 
     # Check if user is transformed, and send their messages as webhooks, deleting the original
-    with open("cache/transformed.txt", "r") as f1:
-        if message.author.name in f1.read():
-            tf_data = utils.load_tf(message.author)
-            name = tf_data["into"]
-            avatar_url = tf_data["image_url"]
+    if utils.is_transformed(message.author, message.guild):
+        tf_data = utils.load_tf(message.author, message.guild)
+        name = tf_data["into"]
+        avatar_url = tf_data["image_url"]
 
-            webhook = utils.get_webhook_by_name(await message.channel.webhooks(), name)
-            if not webhook:
-                webhook = await message.channel.create_webhook(name=name)
+        webhook = utils.get_webhook_by_name(await message.channel.webhooks(), name)
+        if not webhook:
+            webhook = await message.channel.create_webhook(name=name)
 
-            if message.content:  # If there's no content and we try to send, it will trigger a 400 error
-                await webhook.send(message.content, username=name, avatar_url=avatar_url)
-            if message.attachments:  # Send attachments too, even if in separate messages
-                for attachment in message.attachments:
-                    await webhook.send(file=await attachment.to_file(), username=name, avatar_url=avatar_url)
-            if message.stickers:
-                await message.author.send("Sorry, but we don't support stickers, at the moment! :(")
-            await message.delete()
+        if message.content:  # If there's no content and we try to send, it will trigger a 400 error
+            await webhook.send(message.content, username=name, avatar_url=avatar_url)
+        if message.attachments:  # Send attachments too, even if in separate messages
+            for attachment in message.attachments:
+                await webhook.send(file=await attachment.to_file(), username=name, avatar_url=avatar_url)
+        if message.stickers:
+            await message.author.send("Sorry, but we don't support stickers, at the moment! :(")
+        await message.delete()
 
 
 # Reaction added
@@ -69,13 +65,14 @@ async def on_message(message: discord.Message):
 async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
     # Check if reaction is reacting to a message sent by a transformed user
     if str(reaction.emoji) == "❓":
-        with open("cache/transformed.txt", "r") as f1:
-            for line in f1.readlines():
-                tf_data = utils.load_tf_by_name(line.strip())
-                if tf_data["into"] == reaction.message.author.name:
-                    await user.send(f"*{reaction.message.author.name}* is, in reality,"
-                                    f"*{line.strip()}*, who has been transformed, by *{tf_data['transformed_by']}*!")
-                    await reaction.remove(user)
+        transformed = utils.get_transformed(reaction.message.guild)
+        for tfed in transformed:
+            tf_data = utils.load_tf_by_name(tfed, reaction.message.guild)
+            if tf_data["into"] == reaction.message.author.name:
+                await user.send(f"*{reaction.message.author.name}* is, in fact, *{tfed}*!\n"
+                                f"(Transformed by *{tf_data['transformed_by']}*)")
+                await reaction.remove(user)
+
 
 # Transformation commands
 @bot.slash_command(description="Transform someone")
@@ -107,41 +104,27 @@ async def goback(ctx: discord.ApplicationContext,
                  user: discord.Option(discord.User) = None):
     if user is None:
         user = ctx.author
+    into = utils.load_tf(user, ctx.guild)["into"]
 
-    with open("cache/transformed.txt", "r+") as f:
-        lines = f.readlines()
+    if not utils.is_transformed(user, ctx.guild):
+        if into == "":
+            return await ctx.respond(f"{user.mention} is not transformed at the moment, and has no form to go back to!")
+        utils.write_transformed(user, ctx.guild)
+        return await ctx.respond(f"{user.mention} has been turned back to their last form!")
 
-        # Empty the file
-        f.seek(0)
-        f.truncate()
-
-        found = False
-        for line in lines:
-            if line.strip() != user.name:
-                f.write(line)
-            else:
-                found = True
-        if not found:
-            with open(f"cache/people/{user.name}.json", "r") as f2:
-                if f2.read().strip() == "":
-                    return await ctx.respond(
-                        f"{user.mention} is not transformed at the moment, and has no form to go back to!")
-            f.write(user.name)
-            f.write("\n")
-            return await ctx.respond(f"{user.mention} has been turned back to their last form!")
+    utils.remove_transformed(user, ctx.guild)
 
     # Delete all webhooks with the same name
     # This can lead to deleting more webhooks than we need to, but it shouldn't cause too much of a performance hit
     for wh in await ctx.guild.webhooks():
-        if wh.name == utils.load_tf(user)["into"]:
+        if wh.name == into:
             await wh.delete()
     await ctx.respond(f"{user.mention} has been turned back to normal!")
 
 
 @bot.slash_command(description="Get a list of transformed users")
 async def listtransformed(ctx: discord.ApplicationContext):
-    with open("cache/transformed.txt", "r") as f:
-        transformed = f.readlines()
+    transformed = utils.get_transformed(ctx.guild)
     if not transformed:
         return await ctx.respond("No one is transformed at the moment!")
     await ctx.respond("The following people are transformed at the moment:\n" + "".join(transformed))
--- utils.py
+++ utils.py
@@ -1,8 +1,14 @@
 import json
+import os
 
 import discord
 
-CURRENT_DATA_VERSION = 1
+CURRENT_TFEE_DATA_VERSION = 2
+# VERSION 1: Base version
+# VERSION 2: Added guild specific data
+
+CURRENT_TRANSFORMED_DATA_VERSION = 1
+# VERSION 1: Base version
 
 
 def get_webhook_by_name(webhooks, name) -> discord.Webhook or None:
@@ -12,17 +18,28 @@ def get_webhook_by_name(webhooks, name) -> discord.Webhook or None:
     return None
 
 
-def load_tf(user: discord.User) -> dict:
-    with open(f"cache/people/{user.name}.json", "r") as f:
-        return json.loads(f.read())
-
-
-def load_tf_by_name(name: str) -> dict:
+def load_tf_by_name(name: str, guild: discord.Guild = None) -> dict:
+    if f"{name}.json" not in os.listdir("cache/people"):
+        return {}
     with open(f"cache/people/{name}.json", "r") as f:
-        return json.loads(f.read())
+        contents = f.read()
+        if contents.strip() == "":
+            return {}
+        if guild is None:
+            return json.loads(contents)
+        else:
+            data = json.loads(contents)
+            if str(guild.id) in data:
+                return data[str(guild.id)]
+            return {}
+
+
+def load_tf(user: discord.User, guild: discord.Guild = None) -> dict:
+    return load_tf_by_name(user.name, guild)
 
 
 def write_tf(user: discord.User,
+             guild: discord.Guild,
              transformed_by: str = None,
              into: str = None,
              image_url: str = None,
@@ -36,9 +53,35 @@ def write_tf(user: discord.User,
              sprinkle: str = None,
              muffle_bool: bool = False,
              muffle: str = None) -> None:
+    data = load_tf(user)
+    if data == {}:  # If the file is empty, we need to add the version info
+        data["version"] = CURRENT_TFEE_DATA_VERSION
+    elif data["version"] != CURRENT_TFEE_DATA_VERSION:
+        data["version"] = CURRENT_TFEE_DATA_VERSION
+        if data["version"] < 2:
+            data[guild.id] = {
+                "transformed_by": data["transformed_by"],
+                "into": data["into"],
+                "image_url": data["image_url"],
+                "claim": data["claim"],
+                "eternal": data["eternal"],
+                "prefix": data["prefix"],
+                "suffix": data["suffix"],
+                "censor": {
+                    "active": data["censor"]["active"],
+                    "contents": data["censor"]["contents"]
+                },
+                "sprinkle": {
+                    "active": data["sprinkle"]["active"],
+                    "contents": data["sprinkle"]["contents"]
+                },
+                "muffle": {
+                    "active": data["muffle"]["active"],
+                    "contents": data["muffle"]["contents"]
+                }
+            }
     if into not in ["", None]:
-        data = {
-            "version": CURRENT_DATA_VERSION,
+        data[str(guild.id)] = {  # Add guild specific data
             "transformed_by": transformed_by,
             "into": into,
             "image_url": image_url,
@@ -60,46 +103,91 @@ def write_tf(user: discord.User,
             }
         }
     else:
-        data = load_tf(user)
-        # Add all the new data
-        # Could this be made more efficient? Probably, but I'm too lazy to do it right now
-        # TODO: Improve this code.
-
-        # Check if the version is correct
-        # if data["version"] != CURRENT_DATA_VERSION:
-        # Here we would make sure any changes that haven't been implemented yet on older versions are implemented
-
         if transformed_by:
-            data["transformed_by"] = transformed_by
+            data[str(guild.id)]["transformed_by"] = transformed_by
         if image_url:
-            data["image_url"] = image_url
+            data[str(guild.id)]["image_url"] = image_url
         if claim_user:
-            data["claim"] = claim_user.strip()
+            data[str(guild.id)]["claim"] = claim_user.strip()
         elif claim_user.strip() == "":
-            data["claim"] = None
+            data[str(guild.id)]["claim"] = None
         if eternal:
-            data["eternal"] = eternal
+            data[str(guild.id)]["eternal"] = eternal
         if prefix:
-            data["prefix"] = prefix
+            data[str(guild.id)]["prefix"] = prefix
         if suffix:
-            data["suffix"] = suffix
+            data[str(guild.id)]["suffix"] = suffix
         if censor:
-            data["censor"]["active"] = True
-            data["censor"]["contents"] = censor.strip()
+            data[str(guild.id)]["censor"]["active"] = True
+            data[str(guild.id)]["censor"]["contents"] = censor.strip()
         elif censor.strip() == "":
-            data["censor"]["active"] = False
-            data["censor"]["contents"] = None
+            data[str(guild.id)]["censor"]["active"] = False
+            data[str(guild.id)]["censor"]["contents"] = None
         if sprinkle:
-            data["sprinkle"]["active"] = True
-            data["sprinkle"]["contents"] = sprinkle.strip()
+            data[str(guild.id)]["sprinkle"]["active"] = True
+            data[str(guild.id)]["sprinkle"]["contents"] = sprinkle.strip()
         elif sprinkle.strip() == "":
-            data["sprinkle"]["active"] = False
-            data["sprinkle"]["contents"] = None
+            data[str(guild.id)]["sprinkle"]["active"] = False
+            data[str(guild.id)]["sprinkle"]["contents"] = None
         if muffle:
-            data["muffle"]["active"] = True
-            data["muffle"]["contents"] = muffle.strip()
+            data[str(guild.id)]["muffle"]["active"] = True
+            data[str(guild.id)]["muffle"]["contents"] = muffle.strip()
         elif muffle.strip() == "":
-            data["muffle"]["active"] = False
-            data["muffle"]["contents"] = None
+            data[str(guild.id)]["muffle"]["active"] = False
+            data[str(guild.id)]["muffle"]["contents"] = None
     with open(f"cache/people/{user.name}.json", "w+") as f:
-        f.write(json.dumps(data, indent=4)) # Indents are just so that data is more readable. Remove for production.
+        f.write(json.dumps(data, indent=4))  # Indents are just so that data is more readable. Remove for production.
+
+
+def remove_tf(user: discord.User, guild: discord.Guild) -> None:
+    data = load_tf(user)
+    del data[guild.id]
+    with open(f"cache/people/{user.name}.json", "w+") as f:
+        f.write(json.dumps(data, indent=4))  # Indents are just so that data is more readable. Remove for production.
+
+
+def remove_all_tf(user: discord.User) -> None:
+    os.remove(f"cache/people/{user.name}.json")
+
+
+def load_transformed(guild: discord.Guild = None) -> dict:
+    if "transformed.json" not in os.listdir("cache"):
+        return {}
+    with open("cache/transformed.json", "r") as f:
+        contents = f.read()
+        if contents == "":
+            return {}
+        data = json.loads(contents)
+        if guild is None:
+            return data
+        else:
+            if str(guild.id) in data:
+                return data[str(guild.id)]
+            return {}
+
+
+def write_transformed(user: discord.User, guild: discord.Guild) -> None:
+    data = load_transformed()
+    if data == {}:  # If the file is empty, we need to add the version info
+        data["version"] = CURRENT_TRANSFORMED_DATA_VERSION
+    if guild.id not in data:
+        data[str(guild.id)] = []
+    if user.name not in data[str(guild.id)]:
+        data[str(guild.id)].append(user.name)
+    with open("cache/transformed.json", "w+") as f:
+        f.write(json.dumps(data, indent=4))  # Indents are just so that data is more readable. Remove for production.
+
+
+def remove_transformed(user: discord.User, guild: discord.Guild) -> None:
+    data = load_transformed()
+    data[str(guild.id)].remove(user.name)
+    with open("cache/transformed.json", "w+") as f:
+        f.write(json.dumps(data, indent=4))  # Indents are just so that data is more readable. Remove for production.
+
+
+def is_transformed(user: discord.User, guild: discord.Guild) -> bool:
+    return user.name in load_transformed(guild)
+
+
+def get_transformed(guild: discord.Guild) -> dict:
+    return load_transformed(guild)
