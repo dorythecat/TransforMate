dorythecat/TransforMate

commit e031a1cef1d0c77787da1a2de540e9b92394951b

Fixed undocumented bug
If using "/transfrom" or "/transform (user)", the bot would transform the user, but not notify them of such. Now it does. Yipee!

main.py
@@ -101,6 +101,7 @@ async def transform(ctx: discord.ApplicationContext,
                              user,
                              response.content,
                              response.attachments[0].url if response.attachments else None)
+    await ctx.respond(f'You have transformed {user.mention} into "{into}"!')
 
 
 @bot.slash_command(description="Return someone to their previous state")

commit 4400fef79230e4605961db86065d8a1b8d552afe

Fixed an undocumented bug
Turns out the bug-fixing at e031a1cef1d0c77787da1a2de540e9b92394951b, caused a bug of its own. Now, the message would always read "You have transformed (user) into "None"". No longwer does that, you'll thank me later.

main.py
@@ -101,7 +101,7 @@ async def transform(ctx: discord.ApplicationContext,
                              user,
                              response.content,
                              response.attachments[0].url if response.attachments else None)
-    await ctx.respond(f'You have transformed {user.mention} into "{into}"!')
+    await ctx.respond(f'You have transformed {user.mention} into "{response.content}"!')
 
 
 @bot.slash_command(description="Return someone to their previous state")