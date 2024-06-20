a306232cf33278a57ccfc2095aee83361cd296b7
Dory <pemegomachado@gmail.com>
2024-06-20 14:46:21 +0200

    Added /block user command

--- src/cogs/block.py
+++ src/cogs/block.py
@@ -22,6 +22,14 @@ class Block(commands.Cog):
         channel_word = "this channel" if channel == ctx.channel else channel.mention
         await ctx.respond(f"You will now be {word} in {channel_word}!")
 
+    @block_command.command(description="Block a user from interacting with you")
+    async def user(self,
+                   ctx: discord.ApplicationContext,
+                   user: discord.User) -> None:
+        utils.write_tf(ctx.author, ctx.guild, block_user=user)
+        word = "unblocked" if user.id in utils.load_tf(ctx.user, ctx.guild)['blocked_users'] else "blocked"
+        await ctx.respond(f"{user.mention} has been {word} from interacting with you!")
+
 
 def setup(bot: discord.Bot) -> None:
     bot.add_cog(Block(bot))
