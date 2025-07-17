91524db67f60b399d898f361ce2e645407d58c5a
Dory <pemegomachado@gmail.com>
2025-07-17 14:08:16 +0200

    Fixed undocumented error

--- src/cogs/get.py
+++ src/cogs/get.py
@@ -141,7 +141,7 @@ class Get(commands.Cog):
             transformed_data = transformed_data[
                 str(ctx.channel.id) if str(ctx.channel.id) in transformed_data else 'all']
             into = transformed_data['into']
-            description += f"<@{tfee}> ({discord.Bot.get_user(self, int(tfee)).name}) is \"{into}\"\n\n"
+            description += f"<@{tfee}> ({discord.Bot.get_user(self.bot, int(tfee)).name}) is \"{into}\"\n\n"
         # Take off the last two new lines
         description = description[:-2]
         await ctx.respond(embed=utils.get_embed_base(title="Transformed Users", desc=description))
