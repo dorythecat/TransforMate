ca65fa8eeae863d90c31af74dad15183e4906b3c
Dory <pemegomachado@gmail.com>
2025-07-17 13:53:42 +0200

    Reduced mention on /get transformed

--- src/cogs/get.py
+++ src/cogs/get.py
@@ -141,9 +141,7 @@ class Get(commands.Cog):
             transformed_data = transformed_data[
                 str(ctx.channel.id) if str(ctx.channel.id) in transformed_data else 'all']
             into = transformed_data['into']
-            if ctx.guild.get_member(int(tfee)) is None:
-                continue
-            description += f"{ctx.guild.get_member(int(tfee)).mention} is \"{into}\"\n\n"
+            description += f"<@{tfee}> is \"{into}\"\n\n"
         # Take off the last two new lines
         description = description[:-2]
         await ctx.respond(embed=utils.get_embed_base(title="Transformed Users", desc=description))
