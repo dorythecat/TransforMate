1ca9496acd9991a5226c2f063399d49c67d13261
Dory <pemegomachado@gmail.com>
2025-07-26 13:16:30 +0200

    Updated a bit of code

--- src/api.py
+++ src/api.py
@@ -17,10 +17,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# to get a string like this run:
-# openssl rand -hex 32
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = "HS256" # Algorith for JWT to use to encode tokens
+ACCESS_TOKEN_EXPIRE_HOURS = 2 # After how many hours does the access token expire automatically
 
 
 fake_users_db = {
@@ -117,7 +115,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
             detail="Incorrect username or password!",
             headers={"WWW-Authenticate": "Bearer"}
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return Token(access_token=access_token, token_type="bearer")
 
