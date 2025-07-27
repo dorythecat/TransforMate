c5de73e63c10f885638927f19d4c989149919a56
Dory <pemegomachado@gmail.com>
2025-07-27 16:59:34 +0200

    Added transformation through the API

--- src/api.py
+++ src/api.py
@@ -12,6 +12,7 @@ from pydantic import BaseModel
 
 import utils
 from config import BLOCKED_USERS, CACHE_PATH, SECRET_KEY
+from cogs.transformation import transform_function
 
 # Setting some basic things up
 app = FastAPI()
@@ -184,6 +185,182 @@ def get_tfed_user(current_user: Annotated[User, Depends(get_current_active_user)
         )
 
     return tf
+
+# Transform other users
+class TransformationData(BaseModel):
+    into: str | None = None
+    image_url: str | None = None
+    channel_id: int | None = None
+    brackets: list[str] | None = None
+    copy_id: int | None = None
+    merge: bool = False
+
+@app.put("/tf/{server_id}/{user_id}")
+def tf_user(current_user: Annotated[User, Depends(get_current_active_user)],
+            server_id: int,
+            user_id: int,
+            tf_data: Annotated[TransformationData, Depends()]) -> dict:
+    if server_id not in current_user.in_servers:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not in this server!",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    server = utils.load_transformed(server_id)
+    if server != {}:
+        if str(current_user.linked_id) in server['blocked_users']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="This server has blocked you!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+
+        if str(user_id) in server['blocked_users']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="This server has blocked the user!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+
+        if str(tf_data.channel_id) in server['blocked_channels']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="This server has blocked this channel!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+
+    tf = utils.load_tf_by_id(str(user_id), server_id)
+    if tf != {}:
+        if str(current_user.linked_id) in tf['blocked_users']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="This user has blocked you!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+        if str(user_id) in tf['blocked_users']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="This user has blocked you!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+
+    if utils.is_transformed(user_id, server_id):
+        if str(tf_data.channel_id) in tf:
+            tf = tf[str(tf_data.channel_id)]
+        elif 'all' in tf:
+            tf = tf['all']
+        elif server != {} and server['affixes']:
+            tf = {'claim': None}  # Empty data so we can do multiple tfs
+        elif tf == {}:
+            # This is to avoid https://github.com/dorythecat/TransforMate/issues/25
+            tf = {'claim': None}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="This user is already transformed on this server!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+        if tf['claim'] is not None and int(tf['claim']) != current_user.linked_id and tf['eternal']:
+            if current_user.linked_id != user_id:
+                raise HTTPException(
+                    status_code=status.HTTP_409_CONFLICT,
+                    detail="This user is claimed, and you aren't their owner!",
+                    headers={"WWW-Authenticate": "Bearer"}
+                )
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="You are claimed, and can't transform yourself!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+
+    if server != {} and server['affixes']:
+        if not tf_data.brackets:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="You need to provide brackets in this server!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+        if len(tf_data.brackets) > 2:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Provided brackets are improperly formed!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+    else:
+        if tf_data.brackets is not None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="You can't provide brackets on this server!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+
+    if tf_data.copy_id is not None:
+        new_data = utils.load_tf_by_id(str(tf_data.copy_id), server_id)
+        if new_data == {} or new_data['all'] == {}:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="This user is no transformed on this server!",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+        if tf_data.merge in [False, None]:
+            new_data['all']['into'] += "឵឵ᅟ"
+        if tf_data.into:
+            # Webhook username cannot contain "discord", or it will return a 400 error
+            # TODO: Find a better fix, perhaps?
+            if tf_data.into.lower().__contains__("discord"):
+                tf_data.into = tf_data.into.lower().replace("discord", "Disc0rd")
+            new_data['all']['into'] = tf_data.into
+        if tf_data.image_url:
+            tf_data.image_url = tf_data.image_url.strip()
+            if tf_data.image_url[:4] != "http":
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Image URL must start with http!",
+                    headers={"WWW-Authenticate": "Bearer"}
+                )
+            if "?" in tf_data.image_url:  # Prune url, if possible, to preserve space
+                tf_data.image_url = tf_data.image_url[:tf_data.image_url.index("?")]
+            new_data['all']['image_url'] = tf_data.image_url
+        utils.write_tf(user_id, server_id, new_data=new_data)
+        utils.write_transformed(server_id, user_id)
+        return utils.load_tf_by_id(str(user_id), server_id)
+
+    if tf_data.into:
+        if len(tf_data.into) <= 1:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Name needs to be at least wo characters long!",
+            )
+        if not tf_data.image_url:
+            tf_data.image_url = "https://cdn.discordapp.com/embed/avatars/1.png"
+        else:
+            tf_data.image_url = tf_data.image_url.strip()
+            if tf_data.image_url[:4] != "http":
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Invalid Image URL! Please provide a valid image URL!",
+                    headers={"WWW-Authenticate": "Bearer"}
+                )
+            if "?" in tf_data.image_url:  # Prune url, if possible, to preserve space
+                tf_data.image_url = tf_data.image_url[:tf_data.image_url.index("?")]
+
+        # Webhook username cannot contain "discord", or it will return a 400 error
+        # TODO: Find a better fix, perhaps?
+        if tf_data.into.lower().__contains__("discord"):
+            tf_data.into = tf_data.into.lower().replace("discord", "Disc0rd")
+
+        utils.write_tf(user_id,
+                       server_id,
+                       tf_data.channel_id,
+                       transformed_by=str(current_user.linked_id),
+                       into=tf_data.into.strip(),
+                       image_url=tf_data.image_url,
+                       proxy_prefix=tf_data.brackets[0] if tf_data.brackets is not None else None,
+                       proxy_suffix=tf_data.brackets[1] if tf_data.brackets is not None else None)
+        utils.write_transformed(server_id, user_id, tf_data.channel_id)
+
+    return utils.load_tf_by_id(str(user_id), server_id)
 
 # User-related features
 @app.get("/users/me", response_model=User)
