ca1b4e21fc4f73d73c4784b5205e39768fba5862
Dory <pemegomachado@gmail.com>
2025-08-02 22:45:08 +0200

    Fixed an issue

--- src/api.py
+++ src/api.py
@@ -66,7 +66,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 ALGORITHM = "HS256" # Algorith for JWT to use to encode tokens
