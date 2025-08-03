8272b3f31db1e1a01b0ae2b812ad25dfef638f6d
Dory <pemegomachado@gmail.com>
2025-08-03 22:06:04 +0200

    Added basic editor

--- web/res/script.js
+++ web/res/script.js
@@ -45,46 +45,64 @@ function encode_tsf(into,
     data.push(backwards ? "1" : "0");
 
     data.push(stutter.toString());
-    data.push(proxy_prefix);
-    data.push(proxy_suffix);
-    data.push(bio);
+    data.push(proxy_prefix != null ? proxy_prefix : "");
+    data.push(proxy_suffix != null ? proxy_suffix : "");
+    data.push(bio != null ? bio : "");
 
-    data.push(prefixes ? "1" : "0");
-    if (prefixes) {
+    if (prefixes != null && prefixes.length > 0) {
+        data.push("1");
         let prefix_data = []
-        for (let prefix in prefixes) prefix_data.push(prefix + "|" + prefixes[prefix]);
+        for (let prefix of prefixes) prefix_data.push(prefix.content + "|" + prefix.value);
         data.push(prefix_data.join(","));
-    } else data.push("");
-    data.push(suffixes ? "1" : "0");
-    if (suffixes) {
+    } else {
+        data.push("0");
+        data.push("");
+    }
+    if (suffixes != null && suffixes.length > 0) {
+        data.push("1");
         let suffix_data = []
-        for (let suffix in suffixes) suffix_data.push(suffix + "|" + suffixes[suffix]);
+        for (let suffix of suffixes) suffix_data.push(suffix.content  + "|" + suffix.value);
         data.push(suffix_data.join(","));
-    } else data.push("");
-    data.push(sprinkles ? "1" : "0");
-    if (sprinkles) {
+    } else {
+        data.push("0");
+        data.push("");
+    }
+    if (sprinkles != null && sprinkles.length > 0) {
+        data.push("1");
         let sprinkle_data = []
-        for (let sprinkle in sprinkles) sprinkle_data.push(sprinkle + "|" + sprinkles[sprinkle]);
+        for (let sprinkle of sprinkles) sprinkle_data.push(sprinkle.content + "|" + sprinkle.value);
         data.push(sprinkle_data.join(","));
-    } else data.push("");
-    data.push(muffles ? "1" : "0");
-    if (muffles) {
+    } else {
+        data.push("0");
+        data.push("");
+    }
+    if (muffles != null && muffles.length > 0) {
+        data.push("1");
         let muffle_data = []
-        for (let muffle in muffles) muffle_data.push(muffle + "|" + muffles[muffle]);
+        for (let muffle of muffles) muffle_data.push(muffle.content + "|" + muffle.value);
         data.push(muffle_data.join(","));
-    } else data.push("");
-    data.push(alt_muffles ? "1" : "0");
-    if (alt_muffles) {
+    } else {
+        data.push("0");
+        data.push("");
+    }
+    if (alt_muffles != null && alt_muffles.length > 0) {
+        data.push("1");
         let alt_muffle_data = []
-        for (let alt_muffle in alt_muffles) alt_muffle_data.push(alt_muffle + "|" + alt_muffles[alt_muffle]);
+        for (let alt_muffle of alt_muffles) alt_muffle_data.push(alt_muffle.content + "|" + alt_muffle.value);
         data.push(alt_muffle_data.join(","));
-    } else data.push("");
-    data.push(censors ? "1" : "0");
-    if (censors) {
+    } else {
+        data.push("0");
+        data.push("");
+    }
+    if (censors != null && censors.length > 0) {
+        data.push("1");
         let censor_data = []
-        for (let censor in censors) censor_data.push(censor + "|" + censors[censor]);
+        for (let censor of censors) censor_data.push(censor.content + "|" + censor.value);
         data.push(censor_data.join(","));
-    } else data.push("");
+    } else {
+        data.push("0");
+        data.push("");
+    }
     return data.join(";");
 }
 
@@ -114,11 +132,194 @@ logout.onclick = function (e) {
 
 // TSF Editor page
 if (window.location.href.includes("tsf_editor.html")) {
-    new_tf_name = document.getElementById("new_tf_name");
-    new_tf_img = document.getElementById("new_tf_img");
-    new_tf_submit = document.getElementById("new_tf_submit");
+    const new_tf_name = document.getElementById("new_tf_name");
+    const new_tf_img = document.getElementById("new_tf_img");
+    const new_tf_submit = document.getElementById("new_tf_submit");
+    const new_tf_container = document.getElementById("new_tf_container");
+    const tf_data_form = document.getElementById("tf_data_form");
 
     new_tf_submit.onclick = function (e) {
-        console.log(encode_tsf(new_tf_name.value, new_tf_img.value));
+        if (new_tf_name.value === "" || new_tf_img.value === "") {
+            alert("Please fill out all required fields!");
+            return;
+        }
+        if (new_tf_name.value.length < 2) {
+            alert("Name must be at least 2 characters long!");
+            return;
+        }
+        if (new_tf_img.value.length < 15 || new_tf_img.value.length > 1024 || !new_tf_img.value.startsWith("http")) {
+            alert("Image URL must be a valid URL!");
+            return;
+        }
+        new_tf_container.style.width = "100%";
+        tf_data_form.style.display = "inline";
+    }
+
+    // Form data
+    const big = document.getElementById("big");
+    const small = document.getElementById("small");
+    const hush = document.getElementById("hush");
+    const backwards = document.getElementById("backwards");
+    const bio = document.getElementById("bio");
+
+    // Set the sliders to sync with their respective values and viceversa
+    const sliderPairs = [
+        { // Stutter
+            slider: document.getElementById("stutter"),
+            value: document.getElementById("stutter_value")
+        },
+        { // Prefix
+            slider: document.getElementById('prefix_chance'),
+            value: document.getElementById('prefix_chance_value')
+        },
+        { // Suffix
+            slider: document.getElementById('suffix_chance'),
+            value: document.getElementById('suffix_chance_value')
+        },
+        { // Sprinkles
+            slider: document.getElementById('sprinkle_chance'),
+            value: document.getElementById('sprinkle_chance_value')
+        },
+        { // Muffles
+            slider: document.getElementById('muffle_chance'),
+            value: document.getElementById('muffle_chance_value')
+        },
+        { // Alt Muffles
+            slider: document.getElementById('alt_muffle_chance'),
+            value: document.getElementById('alt_muffle_chance_value')
+        }
+    ];
+
+    sliderPairs.forEach(pair => {
+        // Sync slider to value input
+        pair.slider.addEventListener("input", (event) => {
+            pair.value.value = event.target.value;
+        });
+
+        // Sync value input to slider
+        pair.value.addEventListener("input", (event) => {
+            pair.slider.value = event.target.value;
+        });
+    });
+
+    const listConfigs = {
+        'prefixes': {
+            list: [],
+            container: document.getElementById('prefix-container'),
+            contentInput: document.getElementById('prefix-content'),
+            chancePair: sliderPairs[1]
+        },
+        'suffixes': {
+            list: [],
+            container: document.getElementById('suffix-container'),
+            contentInput: document.getElementById('suffix-content'),
+            chancePair: sliderPairs[2]
+        },
+        'sprinkles': {
+            list: [],
+            container: document.getElementById('sprinkle-container'),
+            contentInput: document.getElementById('sprinkle-content'),
+            chancePair: sliderPairs[3]
+        },
+        'muffles': {
+            list: [],
+            container: document.getElementById('muffle-container'),
+            contentInput: document.getElementById('muffle-content'),
+            chancePair: sliderPairs[4]
+        },
+        'alt_muffles': {
+            list: [],
+            container: document.getElementById('alt-muffle-container'),
+            contentInput: document.getElementById('alt-muffle-content'),
+            chancePair: sliderPairs[5]
+        },
+        'censors': {
+            list: [],
+            container: document.getElementById('censor-container'),
+            contentInput: document.getElementById('censor-content'),
+            replacementInput: document.getElementById('censor-replacement')
+        }
+    };
+
+    const listButtons = {
+        'prefixes': document.getElementById('add-prefix-btn'),
+        'suffixes': document.getElementById('add-suffix-btn'),
+        'sprinkles': document.getElementById('add-sprinkle-btn'),
+        'muffles': document.getElementById('add-muffle-btn'),
+        'alt_muffles': document.getElementById('add-alt-muffle-btn'),
+        'censors': document.getElementById('add-censor-btn')
+    }
+
+    window.removeFunction = (index, ID) => {
+        listConfigs[ID].list.splice(index, 1);
+        updateList(ID);
+    };
+
+    function updateList(ID, censor = false) {
+        const { list, container } = listConfigs[ID];
+        container.innerHTML = '';
+
+        list.forEach((item, index) => {
+            const li = document.createElement('li');
+            li.className = 'item';
+            li.innerHTML = `
+                <span>${item.content} (${item.value}${censor ? "" : "%"})</span>
+                <button type="button" onclick="removeFunction(${index}, '${ID}')">Remove</button>
+            `;
+            container.appendChild(li);
+        });
+    }
+
+    for (const [ID, button] of Object.entries(listButtons)) {
+        button.addEventListener('click', () => {
+            const config = listConfigs[ID];
+
+            let valueInput = ID === 'censors' ? config.replacementInput.value : config.chancePair.slider.value
+            if (!config.contentInput.value || !valueInput) return;
+
+            const item = {
+                content: config.contentInput.value,
+                value: valueInput
+            }
+
+            config.list.push(item);
+            updateList(ID, ID === 'censors');
+
+            // Reset inputs
+            config.contentInput.value = '';
+            if (ID === 'censors') config.replacementInput.value = '';
+            else {
+                config.chancePair.slider.value = 30;
+                config.chancePair.value.value = 30;
+            }
+        });
+    }
+
+    // Censor has to be handled separately due to it being a string-string pair
+    
+
+    const submit_tf_btn = document.getElementById("submit_tf_btn");
+
+    submit_tf_btn.onclick = function (e) {
+        const tsf_data = encode_tsf(
+            new_tf_name.value,
+            new_tf_img.value,
+            big.checked,
+            small.checked,
+            hush.checked,
+            backwards.checked,
+            parseInt(document.getElementById("stutter_value").value),
+            null,
+            null,
+            bio.value,
+            listConfigs.prefixes.list,
+            listConfigs.suffixes.list,
+            listConfigs.sprinkles.list,
+            listConfigs.muffles.list,
+            listConfigs.alt_muffles.list,
+            listConfigs.censors.list
+        )
+
+        console.log(tsf_data);
     }
 }
