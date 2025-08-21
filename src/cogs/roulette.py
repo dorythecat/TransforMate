3267b6eb6a404d5681563c60221c57d7eb04fd22
Dory <pemegomachado@gmail.com>
2025-08-21 16:39:27 +0200

    Added preliminary check to roulette commands

--- src/cogs/roulette.py
+++ src/cogs/roulette.py
@@ -26,6 +26,9 @@ class Roulette(commands.Cog):
                                             description="The TSF-compliant transformation string for the item."),
                        name: discord.Option(discord.SlashCommandOptionType.string,
                                             description="The name of the roulette to add an item to.") = "Default") -> None:
+        if utils.load_roulette(name, ctx.guild) == {}:
+            await ctx.respond(f'Roulette "{name}" does not exist!')
+            return
         utils.add_roulette_item(name, ctx.guild, item)
         await ctx.respond(f'Item "{item}" has been added to roulette "{name}"!')
 
@@ -35,6 +38,9 @@ class Roulette(commands.Cog):
                    ctx: discord.ApplicationContext,
                    name: discord.Option(discord.SlashCommandOptionType.string,
                                         description="The name of the roulette to roll.") = "Default") -> None:
+        if utils.load_roulette(name, ctx.guild) == {}:
+            await ctx.respond(f'Roulette "{name}" does not exist!')
+            return
         result = utils.roll_roulette(name, ctx.guild)
         await ctx.respond(f'You rolled: "{result}"!')
 
