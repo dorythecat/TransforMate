c2829c3fd80dd5cb6a33fb2ab56c0bf7e646123c
Dory <pemegomachado@gmail.com>
2025-11-21 15:55:48 +0100

    Replaced get.py with a new behemoth command

--- src/main.py
+++ src/main.py
@@ -15,6 +15,48 @@ intents.message_content = True
 
 bot = discord.Bot(intents=intents)
 
+MAX_ITEMS_PER_PAGE: int = 10 # Max items per page for views
+
+class PageView(discord.ui.View):
+    embed_title: str = ""
+    desc: str = ""
+    total: int = 0
+    offset: int = 0
+
+    def __init__(self, embed_title: str, desc: str, total: int, offset: int = MAX_ITEMS_PER_PAGE) -> None:
+        super().__init__(timeout=None)
+        self.embed_title = embed_title
+        self.desc = desc
+        self.total = total
+        self.offset = offset
+
+    @discord.ui.button(label="⬅️ Previous Page", style=discord.ButtonStyle.primary, disabled=True)
+    async def previous_button_callback(self, button: discord.Button, interaction: discord.Interaction) -> None:
+        if self.offset <= MAX_ITEMS_PER_PAGE:
+            return
+        self.next_button_callback.disabled = False
+        self.offset -= MAX_ITEMS_PER_PAGE * 2
+        desc = "\n\n".join(self.desc.split("\n\n")[self.offset:self.offset + MAX_ITEMS_PER_PAGE])
+        footer = f"Page {self.offset // MAX_ITEMS_PER_PAGE + 1} of {(self.total - 1) // MAX_ITEMS_PER_PAGE + 1}"
+        self.offset += MAX_ITEMS_PER_PAGE
+        await interaction.response.edit_message(embed=utils.get_embed_base(self.embed_title, desc, footer), view=self)
+        if self.offset <= MAX_ITEMS_PER_PAGE:
+            button.disabled = True
+            await interaction.message.edit(view=self)
+
+    @discord.ui.button(label="Next Page ➡️", style=discord.ButtonStyle.primary)
+    async def next_button_callback(self, button: discord.Button, interaction: discord.Interaction) -> None:
+        if self.offset >= self.total:
+            return
+        self.previous_button_callback.disabled = False
+        desc = "\n\n".join(self.desc.split("\n\n")[self.offset:self.offset + MAX_ITEMS_PER_PAGE])
+        footer = f"Page {self.offset // MAX_ITEMS_PER_PAGE + 1} of {(self.total - 1) // MAX_ITEMS_PER_PAGE + 1}"
+        self.offset += MAX_ITEMS_PER_PAGE
+        await interaction.response.edit_message(embed=utils.get_embed_base(self.embed_title, desc, footer), view=self)
+        if self.offset >= self.total:
+            button.disabled = True
+            await interaction.message.edit(view=self)
+
 @bot.event
 async def on_ready() -> None:
     print(f'\nSuccessfully logged into Discord as "{bot.user}"\n')
@@ -336,7 +378,6 @@ async def on_reaction_add(reaction: discord.Reaction, user: discord.User) -> Non
 
 bot.load_extension('.transformation', package='cogs')  # Transformation (base) Commands
 bot.load_extension('.clear', package='cogs')  # "Clear" Commands
-bot.load_extension('.get', package='cogs')  # "Get" Commands
 bot.load_extension('.admin', package='cogs')  # "Admin" Commands
 bot.load_extension('.block', package='cogs')  # "Block" Commands
 
@@ -437,6 +478,85 @@ async def set(ctx: discord.ApplicationContext,
         await ctx.respond(f"{user.mention}'s biography has been set!")
     else:
         await ctx.respond("Please provide a valid modifier type to set!", ephemeral=True)
+
+
+@bot.slash_command(description="Set settings for transformed users")
+async def get(ctx: discord.ApplicationContext,
+              user: discord.Option(discord.User,
+                                   "User to get modifier from, defaults to executor") = None,
+              mod_type: discord.Option(str,
+                                       "The setting to get",
+                                       choices=[
+                                           "all", "claim", "censor", "sprinkle", "muffle", "prefix", "suffix", "bio",
+                                           "image", "tfed_users"
+                                       ]) = None) -> None:
+    valid, data, user = await utils.extract_tf_data(ctx, user, True)
+    if not valid:
+        return
+
+    if mod_type == "all":
+        embed = utils.get_embed_base(f"Settings for {user.name}:")
+        embed.add_field(name="Prefix", value="Yes" if data['prefix'] != {} else "No")
+        embed.add_field(name="Suffix", value="Yes" if data['suffix'] != {} else "No")
+        embed.add_field(name="Big Text", value="Yes" if data['big'] else "No")
+        embed.add_field(name="Small Text", value="Yes" if data['small'] else "No")
+        embed.add_field(name="Hush", value="Yes" if data['hush'] else "No")
+        embed.add_field(name="Backwards", value="Yes" if data['backwards'] else "No")
+        embed.add_field(name="Censor", value="Yes" if data['censor'] != {} else "No")
+        embed.add_field(name="Sprinkle", value="Yes" if data['sprinkle'] != {} else "No")
+        embed.add_field(name="Muffle", value="Yes" if data['muffle'] != {} else "No")
+        embed.add_field(name="Alt Muffle", value="Yes" if data['alt_muffle'] != {} else "No")
+        embed.add_field(name="Stutter", value=f"{data['stutter']}%")
+        await ctx.respond(embed=embed)
+    elif mod_type == "claim":
+        response: str = "isn't claimed by anyone!" if data['claim'] == 0 else f"is claimed by <@!{data['claim']}>!"
+        await ctx.respond(f"{user.mention} {response}")
+    elif mod_type in ["censor", "sprinkle", "muffle", "prefix", "suffix"]:
+        if not data[mod_type]:
+            await ctx.respond(f"{user.mention} has no sprinkles at the moment!")
+            return
+
+        desc: str = ""
+        for mod in data[mod_type]:
+            desc += f"**{mod}**: {data[mod_type][mod]}%\n\n"
+
+        view: PageView | None = None
+        footer: str | None = None
+        mod_name: str = mod_type.capitalize() + ("s" if mod_type not in ["prefix", "suffix"] else "es")
+        if len(data[mod_type]) > MAX_ITEMS_PER_PAGE:
+            view = PageView(f"{mod_name} for {user.name}:", desc, len(data[mod_type]))
+            footer = f"Page 1 of {(len(data[mod_type]) - 1) // MAX_ITEMS_PER_PAGE + 1}"
+        desc = "\n\n".join(desc.split("\n\n")[:MAX_ITEMS_PER_PAGE])
+        await ctx.respond(embed=utils.get_embed_base(f"{mod_name} for {user.name}:", desc, footer), view=view)
+    elif mod_type == "bio":
+        if data['bio'] in ["", None]:
+            await ctx.respond(f"{user.mention} has no biography set!")
+            return
+        await ctx.respond(embed=utils.get_embed_base(f"Biography for {user.name}:", data['bio']))
+    elif mod_type == "image":
+        await ctx.respond(f"{user.mention}'s image for [{data['into']}]({data['image_url']})")
+    elif mod_type == "tfed_users":
+        tfee_data = utils.load_transformed(ctx.guild)['transformed_users']
+        if tfee_data == {}:
+            await ctx.respond("No one is transformed in this server, at the moment!")
+            return
+
+        users: int = 0
+        desc: str = ""
+        for tfee in tfee_data:
+            transformed_data = utils.load_tf(int(tfee), ctx.guild)
+            if transformed_data == {}:
+                continue
+            desc += f"<@!{tfee}> is \"{transformed_data['into']}\"\n\n"
+            users += 1
+
+        view: PageView | None = None
+        footer: str | None = None
+        if users > MAX_ITEMS_PER_PAGE:
+            view = PageView("Transformed Users", desc, users)
+            footer = f"Page 1 of {(users - 1) // MAX_ITEMS_PER_PAGE + 1}"
+        desc = "\n\n".join(desc.split("\n\n")[:MAX_ITEMS_PER_PAGE])
+        await ctx.respond(embed=utils.get_embed_base("Transformed Users", desc, footer), view=view)
 
 # Misc commands
 @bot.slash_command(description="Report a user for misuse of this bot")
